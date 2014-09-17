d21a8da858e03fab94161d731ed18a03629939ce
andreadelrio <delrio.andre@gmail.com>
2014-09-17 16:49:31 -0500

    fixed footer plus enhanced responsiveness

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,9 +19,13 @@
 
 $(document).ready(function() {
 
-	$(function () {
-		$('.example').popover();
-	});
+	var docHeight = $(window).height();
+  var footerHeight = $('#footer').height();
+  var footerTop = $('#footer').position().top + footerHeight;
+
+  if (footerTop < docHeight) {
+    $('#footer').css('margin-top', 10+ (docHeight - footerTop) + 'px');
+  }
 });
 
 
