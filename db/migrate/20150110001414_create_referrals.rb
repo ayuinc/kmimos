2fd1bc869132864dc34dc334bf4eedee9f7540fe
andreadelrio <delrio.andre@gmail.com>
2015-01-20 04:24:09 +0000

    debugging

--- db/migrate/20150110001414_create_referrals.rb
+++ db/migrate/20150110001414_create_referrals.rb
@@ -1,7 +1,7 @@
 class CreateReferrals < ActiveRecord::Migration
   def change
     create_table :referrals do |t|
-      t.string :code_value
+      t.string :code
       t.string :referrer_name
       t.string :referrer_email
 
