c398fb50bb34efa64cd5bd781e5791a7263650f1
andreadelrio <delrio.andre@gmail.com>
2015-04-21 22:52:31 -0400

    added address to admin mailer

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ActiveRecord::Base
 	include Tokenable
 	include ResetTokenable
   belongs_to :provider
-  validates_presence_of :user_email, :user_first_name, :user_last_name, :provider_id, :user_phone, :raza, :edad
+  validates_presence_of :user_email, :user_first_name, :user_last_name, :provider_id, :user_phone, :raza, :edad, :address
   validates_format_of :user_email, with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   def to_param
