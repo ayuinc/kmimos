356a501f24b0ac859b0284d50898caa4e3dda802
Joel Ibaceta <niohnex@gmail.com>
2015-10-21 09:20:48 -0500

    testing files added

---
+++ test/controllers/api/providers_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Api::ProvidersControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/add_user_id_to_testimonies_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AddUserIdToTestimoniesTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
