d8f1866d8e0e2af3ec97b035dbab8b41c25c33c5
Joel Ibaceta <niohnex@gmail.com>
2015-10-28 15:04:58 -0500

    valuation calculation fixed

--- app/models/valuation.rb
+++ app/models/valuation.rb
@@ -7,7 +7,7 @@ class Valuation < ActiveRecord::Base
     response = Array.new
     comments.each do |comment|
       valuations = comment.comment_valuations.where(valuation_id: self.id).map{|val| val.value.to_i}
-      response = [self.name, valuations.reduce(:+) / valuations.size]
+      response = [self.name, valuations.reduce(:+).to_i / valuations.size]
     end
     return response 
   end
