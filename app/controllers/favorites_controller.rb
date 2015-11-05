92ef46828558ad2f42798063565f6d4dc4bc429e
Joel Ibaceta <niohnex@gmail.com>
2015-11-05 01:37:29 -0500

    favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,10 @@
 class FavoritesController < ApplicationController
   def new
-     
+    provider = params[:provider_id]
+    user = current_user.id
+    
+    Favorite.new({provider_id: provider, user_id: user}).save
+    redirect_to 'providers/#{provider}'
   end
   
   def create
