570bdd5fa465292384c485497910cbfd59a904d2
Joel Ibaceta <niohnex@gmail.com>
2015-11-07 01:53:43 -0500

    show

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106174420) do
+ActiveRecord::Schema.define(version: 20151107064223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -214,7 +214,6 @@ ActiveRecord::Schema.define(version: 20151106174420) do
 
   create_table "pets", force: true do |t|
     t.string   "name"
-    t.string   "race"
     t.integer  "age"
     t.float    "size"
     t.float    "weight"
