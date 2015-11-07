e5ec14f444d08bfa423f96d09266bf14f7013bcc
Joel Ibaceta <niohnex@gmail.com>
2015-11-06 19:51:23 -0500

    rutes fixes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ ServihogarRails::Application.routes.draw do
 
   root 'providers#home'
 
-  get 'bookings/booking_resume' => 'bookings#booking_resume'
+    get 'bookings/booking_resume' => 'bookings#booking_resume'
+
 
   resources :providers
   resources :users
