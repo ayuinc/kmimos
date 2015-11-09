728741a0b83591ca2b99301957d8a01735adccab
Joel Ibaceta <niohnex@gmail.com>
2015-11-09 12:56:39 -0500

    redirec solve

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -49,7 +49,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     if resource.class == User
-      session[:previous_url] || root_path
+      session[:previous_url] || '/'
     else
       return '/'
     end
