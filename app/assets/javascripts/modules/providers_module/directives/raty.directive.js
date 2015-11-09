87440a4d3e5d5c94cc8b1c61717a28d2d63f1a2b
Joel Ibaceta <niohnex@gmail.com>
2015-11-09 18:36:52 -0500

    add directuve for raty

---
+++ app/assets/javascripts/modules/providers_module/directives/raty.directive.js
@@ -0,0 +1,11 @@
+providers_module.directive("raty", function() {
+    return {
+        restrict: 'AE',
+        link: function(scope, elem, attrs) {
+            $(elem).raty({score: attrs.score,
+                          starOff : '/assets/icono-hueso-gris.svg',
+                          starOn  : '/assets/icono-hueso-verde.svg'
+                        });
+        }
+    }
+}
