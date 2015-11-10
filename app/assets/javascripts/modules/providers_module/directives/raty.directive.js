43bf57c6fd37f54044783ecf2ba2b5b0c6437fa4
Joel Ibaceta <niohnex@gmail.com>
2015-11-09 19:20:59 -0500

    raty read only property added

--- app/assets/javascripts/modules/providers_module/directives/raty.directive.js
+++ app/assets/javascripts/modules/providers_module/directives/raty.directive.js
@@ -4,7 +4,8 @@ providers_module.directive("raty", function() {
         link: function(scope, elem, attrs) {
             $(elem).raty({score: attrs.score,
                           starOff : '/assets/icono-hueso-gris.svg',
-                          starOn  : '/assets/icono-hueso-verde.svg'
+                          starOn  : '/assets/icono-hueso-verde.svg',
+                          readOnly: true
                         });
         }
     }
