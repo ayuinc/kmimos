e914c81758cd77d6c2cf9cccbfe25bc819fa7cba
Joel Ibaceta <niohnex@gmail.com>
2015-11-10 14:16:24 -0500

    changes

--- app/controllers/provider/sessions_controller.rb
+++ app/controllers/provider/sessions_controller.rb
@@ -4,12 +4,11 @@ class Provider::SessionsController < Devise::SessionsController
   def create
     @provider = Provider.find_for_database_authentication(email: params[:provider][:email].downcase)
     if @provider && @provider.valid_password?(params[:provider][:password])
-      # Sign the user in and redirect to the user's show page.
-      redirect_to '/'
+      sign_in(@provider)
+      redirect_to session[:previous_url] || '/'
     else
       #flash.now[:error] = 'Invalid email/password combination' # Not quite right!
-      render 'new'
-    end
+      render 'new' 
   end
  
 end
--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -5,7 +5,7 @@ class User::SessionsController < Devise::SessionsController
     @user = User.find_for_database_authentication(email: params[:user][:email].downcase)
     if @user && @user.valid_password?(params[:user][:password])
       sign_in(@user)
-      redirect_to '/'
+      redirect_to session[:previous_url] || '/'
     else
       #flash.now[:error] = 'Invalid email/password combination' # Not quite right!
       render 'new'
