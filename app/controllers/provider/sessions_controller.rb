899c324c83e825830e9f43ae0710dd1105303cbf
Joel Ibaceta <niohnex@gmail.com>
2015-11-10 13:50:07 -0500

    session provider fixed

--- app/controllers/provider/sessions_controller.rb
+++ app/controllers/provider/sessions_controller.rb
@@ -2,8 +2,8 @@ class Provider::SessionsController < Devise::SessionsController
    
 
   def create
-    @provider = Provider.find_for_database_authentication(email: params[:user][:email].downcase)
-    if @provider && @provider.valid_password?(params[:user][:password])
+    @provider = Provider.find_for_database_authentication(email: params[:provider][:email].downcase)
+    if @provider && @provider.valid_password?(params[:provider][:password])
       # Sign the user in and redirect to the user's show page.
       redirect_to '/'
     else
@@ -17,8 +17,8 @@ end
 
 
 def create
-  @user = User.find_for_database_authentication(email: params[:user][:email].downcase)
-  if @user && @user.valid_password?(params[:user][:password])
+  @user = User.find_for_database_authentication(email: params[:provider][:email].downcase)
+  if @user && @user.valid_password?(params[:provider][:password])
     sign_in(@user)
     redirect_to '/'
   else
