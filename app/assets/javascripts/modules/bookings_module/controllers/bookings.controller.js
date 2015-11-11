763f9d7514f02af9def2cc305bf2411c90923489
Joel Ibaceta <niohnex@gmail.com>
2015-11-11 17:14:44 -0500

    lint jshint used

--- app/assets/javascripts/modules/bookings_module/controllers/bookings.controller.js
+++ app/assets/javascripts/modules/bookings_module/controllers/bookings.controller.js
@@ -2,34 +2,34 @@
 
 bookings_module.controller('BookingsController', ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
 
+  "use strict";
+    
   $scope.bookService;
-
   $scope.pets_booked = [];
   $scope.services_booked = [];
-
   $scope.minDate;
   $scope.maxDate;
-
   $scope.diff = 0;
-
   $scope.services_booked_total = 0;
   $scope.pets_booked_total = 0;
 
   $scope.get_diff_dates = function () {
 
-    var min = $scope.minDate;
-    var max = $scope.maxDate; 
+    var min, max, diff;
+    
+    min = $scope.minDate;
+    max = $scope.maxDate;
 
     min = String(min).split('/');
     max = String(max).split('/');
-
+    
     min = [min[1], min[0], min[2]].join('/');
     max = [max[1], max[0], max[2]].join('/');
 
     min = Date.parse(min);
     max = Date.parse(max);
-
-    var diff = (max - min) / 1000 / 60 / 60 / 24;
+ 
+    diff = (max - min) / 1000 / 60 / 60 / 24;
  
     $scope.diff = diff;
 
@@ -45,8 +45,6 @@ bookings_module.controller('BookingsController', ['$scope', '$filter', '$http', 
     $scope.pets_booked.splice($scope.pets_booked.indexOf(pet), 1);
   };
 
-
-
   $scope.add_pet = function () {
     var rand = Math.floor(Math.random() * 6000) + 1;
     $scope.pets_booked.push({uniq: rand, pet_id: '', price: 0});
@@ -54,35 +52,40 @@ bookings_module.controller('BookingsController', ['$scope', '$filter', '$http', 
   };
 
   $scope.add_service = function () {
-    var rand = Math.floor(Math.random() * 6000) + 1;
-    var field_name = "booking[booked_services_attributes][" + rand + "][service_id]";
+    var rand, field_name;
+    
+    field_name = "booking[booked_services_attributes][" + rand + "][service_id]";
+    rand = Math.floor(Math.random() * 6000) + 1;
+    
     $scope.services_booked.push({uniq: rand, pet_id: '', service_id: '', price: 0});
     $scope.set_total_services();
   };
 
   $scope.set_total_services = function () {
     var total = 0;
-    angular.forEach($scope.services_booked, function(service, key) {
-      total = total + parseInt(service.price);
+    angular.forEach($scope.services_booked, function (service, key) {
+      total += parseInt(service.price);
     });
     $scope.services_booked_total = total;
   };
 
   $scope.set_total_pets = function () {
     var total = 0;
-    angular.forEach($scope.pets_booked, function(pet, key) {
+    angular.forEach($scope.pets_booked, function (pet, key) {
       total = total + parseInt(pet.price * $scope.diff);
     });
     $scope.pets_booked_total = total;
   };
 
-  $scope.update_pets=function(provider_id){
+  $scope.update_pets = function (provider_id) {
 
-
-    var host_name = document.location.hostname;
-    var port = document.location.port;
-    var url_service = "http://" + host_name + ":" + port + "/api/bookings/get_pet_rate.json"; //local url
-    var n = 0;
+    var host_name, port, url_service, n;
+    
+    host_name = document.location.hostname;
+    port = document.location.port;
+    url_service = "http://" + host_name + ":" + port + "/api/bookings/get_pet_rate.json"; //local url
+    n = 0;
+    
     angular.forEach($scope.pets_booked, function (pet, key) {
 
       $http({
@@ -94,16 +97,18 @@ bookings_module.controller('BookingsController', ['$scope', '$filter', '$http', 
         pet.pet_name = response.data.pet_name;
         $scope.set_total_pets();
       }, function (error) {
-        console.log(error);
+        //console.log(error);
       });
     });
   };
 
-
   $scope.update_services = function (provider_id) {
-    var host_name = document.location.hostname;
-    var port = document.location.port;
-    var url_service = "http://" + host_name + ":" + port + "/api/bookings/get_rate.json"; //local url
+    
+    var host_name, port, url_service;
+    host_name = document.location.hostname;
+    port = document.location.port;
+    url_service = "http://" + host_name + ":" + port + "/api/bookings/get_rate.json"; //local url
+    
     angular.forEach($scope.services_booked, function (service, key) {
 
       $http({
@@ -116,27 +121,10 @@ bookings_module.controller('BookingsController', ['$scope', '$filter', '$http', 
         service.service_name = response.data.service_name;
         $scope.set_total_services();
       }, function (error) {
-        console.log(error);
+        // console.log(error);
       });
 
     });
   };
-
-
-
-
-
-  //$http({
-    //method: 'GET',
-    //url: '/someUrl'
-    //}).then(function successCallback(response) {
-    // this callback will be called asynchronously
-    // when the response is available
-  //}, function errorCallback(response) {
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
-    //});
-
-
 
 }]);
