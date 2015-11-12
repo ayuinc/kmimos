9976925d9b19828775b1d02205944d6965bc433a
Joel Ibaceta <niohnex@gmail.com>
2015-11-12 03:56:06 -0500

    fixes

--- app/assets/javascripts/modules/providers_module/directives/raty.directive.js
+++ app/assets/javascripts/modules/providers_module/directives/raty.directive.js
@@ -1,12 +1,18 @@
 providers_module.directive("raty", function() {
-    return {
-        restrict: 'AE',
-        link: function(scope, elem, attrs) {
-            $(elem).raty({score: attrs.score,
-                          starOff : '/assets/icono-hueso-gris.svg',
-                          starOn  : '/assets/icono-hueso-verde.svg',
-                          readOnly: true
-                        });
-        }
+  return {
+    restrict: 'AE',
+    link: function (scope, elem, attrs) {
+      $(elem).raty({score: function(){
+            if (parseInt(attrs.score) == 0){
+              return 4;
+            } else {
+              return attrs.score;
+            }
+          },
+          starOff : '/assets/icono-hueso-gris.svg',
+          starOn  : '/assets/icono-hueso-verde.svg',
+          readOnly: true
+        });
     }
+  };
 });
