bd9b875867e979c00ecd3a92e7ff92e8b270e28c
Joel Ibaceta <niohnex@gmail.com>
2015-11-12 18:13:40 -0500

    user login solve

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -7,11 +7,12 @@ class User::SessionsController < Devise::SessionsController
 
   def create
     @user = User.find_for_database_authentication(email: params[:user][:email].downcase)
+    
     if @user && @user.valid_password?(params[:user][:password])
       sign_in(@user)
       redirect_to session[:previous_url] || '/'
     else
-      #flash.now[:error] = 'Invalid email/password combination' # Not quite right!
+      @user == User.new(params[:user]) if @user == nil 
       render 'new'
     end
   end
