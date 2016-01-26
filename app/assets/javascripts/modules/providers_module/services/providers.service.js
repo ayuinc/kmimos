5b006cd482137b4a658c0e61758c591475cd74b2
Iván Medina Díaz <medinadiazivan@gmail.com>
2016-01-26 22:35:37 +0000

    Deplegable responsive, fondo de vista de error

--- app/assets/javascripts/modules/providers_module/services/providers.service.js
+++ app/assets/javascripts/modules/providers_module/services/providers.service.js
@@ -1,6 +1,6 @@
 providers_module.factory('ProviderService', function($resource, AGENT){
    
-  var url_service="http://" + AGENT.HOST_NAME + ":" + AGENT.PORT + "/api/providers/get_providers"; 
+  var url_service="http://" + AGENT.HOST_NAME + ":" + AGENT.PORT + "/api/providers/get_providers.json"; 
   
   return $resource(url_service, {}, {get: {method: 'GET', isArray: true}});
   
