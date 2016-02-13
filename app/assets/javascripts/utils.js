df31b5eb6824773ce991c3260b668382a1a7c1c2
Ivan Medina <medinadiazivan@gmail.com>
2016-02-13 09:43:25 -0500

    New video for Kmimos MX

--- app/assets/javascripts/utils.js
+++ app/assets/javascripts/utils.js
@@ -181,7 +181,7 @@ function load_player(playerName, playerId){
   player = new YT.Player(playerId, {
     height: '315',
     width: '560',
-    videoId: '4CkQYZVVyBc',
+    videoId: '_08UJ0aYDCk',
     playerVars: {
         'autoplay': 0,
         'rel': 0,
